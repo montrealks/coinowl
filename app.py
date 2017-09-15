518e4ff59c37e38b263f771bfca7c0fa759b9c08
montrealks <kristifer.szabo@gmail.com>
2017-09-15 12:31:07 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from configs import DEBUG
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def getting_started():
+    return "Hello World"
+
+
+app.run(debug=True) if __name__ == '__main__' and DEBUG else None
+
+
