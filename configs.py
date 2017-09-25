6995e2e53ec60a919c1b0000e038ab5e73ebc19e
kris s <kristifer.szabo@gmail.com>
2017-09-25 19:49:11 +0000

    added env variables to heroku

--- configs.py
+++ configs.py
@@ -1,5 +1,5 @@
 from os import environ
-import keys
+
 
 DEBUG = True
 
@@ -7,6 +7,7 @@ TWILIO_SENDER_NUMBER = "+1 438-795-2675"
 
 
 if not environ.get('webrun'):
+    import keys
     MAILGUN_API = keys.MAILGUN_API
     TWILIO_SID = keys.TWILIO_SID
     TWILIO_TOKEN = keys.TWILIO_TOKEN
