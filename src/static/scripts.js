91b9de02ee365db54afb447518e8281449e732be
kris s <kristifer.szabo@gmail.com>
2017-09-29 03:07:46 +0000

    added email and github social links

--- src/static/scripts.js
+++ src/static/scripts.js
@@ -8,38 +8,38 @@ $(function() {
             names.push(val['name']);
             all.push([val['name'], val['price_usd'], val['price_btc'], val['symbol']]);
         });
-        
-        
+
+
         $('#crypto_chooser input').autocomplete({
             source: names,
             minlength: 0,
-            change: function( event) {
+            change: function(event) {
                 var coin = $(this).val();
-                if(coin === null || ($.inArray(coin, names) == -1))
-                   $(this).val(''); /* clear the value */
-                   $(this).attr('placeholder', 'Please choose a coin from the dropdown');
+                if (coin === null || ($.inArray(coin, names) == -1))
+                    $(this).val(''); /* clear the value */
+                $(this).attr('placeholder', 'Please choose a coin from the dropdown');
             }
         });
-        
+
         $('#crypto_chooser input').focus(function() { $(this).autocomplete("search", " "); });
-        
-    });
-        
-        
-        
+
+    
+
+
+
         $('#crypto_chooser input').on('focusout', function(e) {
             var crypto_name = $(this).val();
             var position = names.indexOf($(this).val());
             var btc_value = parseFloat(all[position][2]);
             var usd_value = parseFloat(all[position][1]);
             var eth_value = (parseFloat(all[position][2]) / all[names.indexOf('Ethereum')][2]).toFixed(5)
-
+    
             $('#chosen_icon').removeClass().addClass('cc ' + all[position][3]);
-
+    
             $('#btc_value').text(btc_value);
             $('#eth_value').text(eth_value);
             $('#usd_value').text('$' + usd_value);
-
+    
         });
     });
 
@@ -55,11 +55,11 @@ $(function() {
             $('#amount_chooser input').focus();
             return;
         }
-        
+
         if (!email_and_sms_validator()) {
             return;
         }
-        
+
         var form_data = $('form').serializeArray()
         var coin_current_price = parseFloat($('#btc_value').text());
         var coin_alert_price = parseFloat($("[name='btc_alert_price']").val());
@@ -78,24 +78,30 @@ $(function() {
                 console.log(error);
             }
         });
-        
+
         // Display last submitted alert info box
         $('#last_submit_message').slideDown().text(alert_message());
 
         // Reset all form fields
         document.getElementById('form').reset();
         $('.currency_conversion_foreground').text('');
-
-        
     });
+    
+    
+     $('#email-container').hover(function() {
+            $('#email').show();
+        });
+    $('#email').parent('li').mouseleave(function(){
+        $('#email').toggle()
+    })
+});
 
-
-function coin_choice_validator(names){
+function coin_choice_validator(names) {
     console.log('hello')
     var chosen_altcoin = $('#crypto_chooser input');
-    if(chosen_altcoin === null || ($.inArray(chosen_altcoin.val(), names) == -1))
-       chosen_altcoin.val(''); /* clear the value */
-       chosen_altcoin.attr('placeholder', 'Please choose a coin from the dropdown');
+    if (chosen_altcoin === null || ($.inArray(chosen_altcoin.val(), names) == -1))
+        chosen_altcoin.val(''); /* clear the value */
+    chosen_altcoin.attr('placeholder', 'Please choose a coin from the dropdown');
 }
 
 // Creates an alert message underneath the tool after an alert is succesfully published
@@ -103,12 +109,12 @@ function alert_message() {
     var form_data = {}
     $('form').serializeArray().map(function(x) { form_data[x.name] = x.value; });
     form_data['btc_price_at_creation'] = parseFloat($('#btc_value').text())
-    
+
     var delivery_methods = "";
     var direction = "";
     var message = "";
 
-    
+
     if (form_data['email'] && form_data['sms']) {
         delivery_methods = "SMS and Email alerts";
     }
@@ -135,10 +141,10 @@ function alert_message() {
 function email_and_sms_validator(email, sms) {
     var regex_email = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     var regex_sms = /^\s*(?:\+?(\d{1,3}))?[- (]*(\d{3})[- )]*(\d{3})[- ]*(\d{4})(?: *[x/#]{1}(\d+))?\s*$/;
-    
+
     var email = $("[name='email']").val();
     var sms = $("[name='sms']").val();
-    
+
     var email_test = regex_email.test(email);
     var sms_test = regex_sms.test(sms)
 
@@ -161,5 +167,5 @@ function email_and_sms_validator(email, sms) {
         $("[name='email']").focus();
         return false;
     };
-    
+
 }
