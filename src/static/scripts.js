d469149f7a33f91c9599d89bb2b96c270808bdb5
kris s <kristifer.szabo@gmail.com>
2017-10-06 14:49:46 +0000

    javascrap form validation

--- src/static/scripts.js
+++ src/static/scripts.js
@@ -1,16 +1,22 @@
 $(function() {
+    
+    
+    // Currency choice buttons and currency icon changer
+    $('#currency_choice button').click(function() {
+        var currency_choice = $(this).text()
+        currency_choice_and_icons(currency_choice)
+    });
+
     // Get altcoin ticker from coinmarketcap
     $.getJSON('https://api.coinmarketcap.com/v1/ticker/', function(data) {
         var names = [];
         var all = [];
-
         $.each(data, function(key, val) {
             names.push(val['name']);
             all.push([val['name'], val['price_usd'], val['price_btc'], val['symbol']]);
         });
 
-
-        $('#crypto_chooser input').autocomplete({
+        $('#crypto_chooser').autocomplete({
             source: names,
             minlength: 0,
             change: function(event) {
@@ -21,43 +27,34 @@ $(function() {
             }
         });
 
-        // $('#crypto_chooser input').focus(function() { $(this).autocomplete("search", " "); });
-
-    
-
-
-
-        $('#crypto_chooser input').on('focusout', function(e) {
+        $('#crypto_chooser').on('focusout', function(e) {
             var crypto_name = $(this).val();
             var position = names.indexOf($(this).val());
             var btc_value = parseFloat(all[position][2]);
             var usd_value = parseFloat(all[position][1]);
             var eth_value = (parseFloat(all[position][2]) / all[names.indexOf('Ethereum')][2]).toFixed(5)
-    
+            get_currency_rates(usd_value);
             $('#chosen_icon').removeClass().addClass('cc ' + all[position][3]);
-    
-            $('#btc_value').text(btc_value);
-            $('#eth_value').text(eth_value);
-            $('#usd_value').text('$' + usd_value);
+
+            $('.btc_value').text(btc_value + " BTC");
+            $('.eth_value').text(eth_value + " ETH");
+            $('.usd_value').text('$' + usd_value + " USD");
+            $('#conversion_amounts_info').text('1 ' + crypto_name + ' =')
             $('#conversion-boxes-helptext').text(crypto_name + ' converts to:')
-    
+
         });
     });
 
 
     // Submit form data
     $('#crypto-alert-create').click(function() {
-        // Ensure that a valid coin has been chosen
-        if ($('#crypto_chooser input').val() === "") {
-            $('#crypto_chooser input').focus();
-            return;
-        }
-        if ($('#amount_chooser input').val() === "") {
-            $('#amount_chooser input').focus();
-            return;
-        }
-
-        if (!email_and_sms_validator()) {
+        // Validate coin and currency boxes not empty
+        if (!coin_and_currency_validaion()) {return;}
+        
+        // Validate email and/or SMS
+        var email_and_sms = email_and_sms_validator()
+        console.log(email_and_sms);
+        if (!email_and_sms['email'] && !email_and_sms['sms']) {
             return;
         }
 
@@ -79,50 +76,68 @@ $(function() {
                 console.log(error);
             }
         });
-
+        // Populate the successful alert info box
+        submit_form_alert_message(email_and_sms['email'], email_and_sms['sms'])
+        
+        
         // Display last submitted alert info box
-        $('#last_submit_message').slideDown().text(alert_message());
+        var succesful_alert_message = alert_created_message();
+        console.log(succesful_alert_message);
+        $('#helper-text').slideDown().text(succesful_alert_message);
 
         // Reset all form fields
         document.getElementById('form').reset();
         // Reset conversion boxes
         $('.currency_conversion_foreground').text('');
+
+        // end submit form data
     });
-    
-    $('[name="btc_alert_price"]').focus(function(){
-        var pos=$('#crypto-chooser-inputs').position();
-        var price_alert_helper = $('#price-helper-alert')
-        price_alert_helper
-        .toggle()
-        .css('top', pos.top - (price_alert_helper.height()/2)); 
-        
+
+    // Site owner social links
+    $('#email-container').hover(function() {
+        $('#email').show();
     });
-    
-     $('#email-container').hover(function() {
-            
-            $('#email').show();
-        });
-    $('#email').parent('li').mouseleave(function(){
+
+    $('#email').parent('li').mouseleave(function() {
         $('#email').toggle();
     });
-    
-    $('[name="email"]').focusout(function(){
+
+    $('[name="email"]').focusout(function() {
         email_and_sms_validator()
-        
     });
-    
 });
 
-function coin_choice_validator(names) {
-    console.log('hello')
-    var chosen_altcoin = $('#crypto_chooser input');
-    if (chosen_altcoin === null || ($.inArray(chosen_altcoin.val(), names) == -1))
-        chosen_altcoin.val(''); /* clear the value */
-    // chosen_altcoin.attr('placeholder', 'Please choose a coin from the dropdown');
+function coin_and_currency_validaion() {
+    if ($('#crypto_chooser input').val() === "") {
+        $('#crypto_chooser input').focus();
+        return false;
+    }
+    if ($('#amount_chooser input').val() === "") {
+        $('#crypto_chooser input').focus();
+        return false;
+    }
+    return true;
+}
+
+function currency_choice_and_icons(currency_choice) {
+    // On press any currency choice button remove the chosen class and then apply it to pressed button
+    $('#currency_choice button').removeClass('currency_choice btn-info');
+    $(this).addClass('currency_choice btn-info');
+
+    // change the icon in the amount chooser to match the chosen currency
+    if (currency_choice == 'USD' || currency_choice == 'CAD') {
+        $('#chosen_currency_icon').removeClass().addClass('glyphicon glyphicon-usd')
+    }
+    else if (currency_choice == 'BTC') {
+        $('#chosen_currency_icon').removeClass().addClass('glyphicon glyphicon-btc')
+    }
+    else {
+        $('#chosen_currency_icon').removeClass().addClass('cc ETH')
+    }
 }
 
 // Creates an alert message underneath the tool after an alert is succesfully published
-function alert_message() {
+function alert_created_message() {
     var form_data = {}
     $('form').serializeArray().map(function(x) { form_data[x.name] = x.value; });
     form_data['btc_price_at_creation'] = parseFloat($('#btc_value').text())
@@ -130,7 +145,7 @@ function alert_message() {
     var delivery_methods = "";
     var direction = "";
     var message = "";
-
+    console.log('inside alert_created_message')
 
     if (form_data['email'] && form_data['sms']) {
         delivery_methods = "SMS and Email alerts";
@@ -152,46 +167,57 @@ function alert_message() {
     message = 'New ' + form_data['coin'] + ' alert created: You will receive ' +
         delivery_methods + ' when ' + form_data['coin'] + ' has ' + direction +
         form_data['btc_alert_price'] + ' BTC';
+    console.log(message);
     return message;
 }
 
 function email_and_sms_validator() {
     var regex_email = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     var regex_sms = /^\s*(?:\+?(\d{1,3}))?[- (]*(\d{3})[- )]*(\d{3})[- ]*(\d{4})(?: *[x/#]{1}(\d+))?\s*$/;
-
     var email = $("[name='email']").val();
     var sms = $("[name='sms']").val();
-    console.log(email + sms)
     var email_test = regex_email.test(email);
     var sms_test = regex_sms.test(sms)
-    
+
+    if (!sms_test && !email_test) {
+        $('#crypto-alert-create').text('Please enter a delivery method to create an alert!')
+        console.log('Both emai and SMS failed validation')
+        return false;
+    }
+    else {
+        $('#crypto-alert-create').text('Create Alert!');
+        console.log('either email or sms or both are valid')
+        return { 'email': email_test, 'sms': sms_test }
+    }
+}
+
+function submit_form_alert_message(validated_contact_data) {
     var direction = ""
     var currency = $('#crypto_chooser input').val();
     var btc_value_now = parseFloat($('#btc_value').text());
     var btc_alert_price = $('[name="btc_alert_price"]').val();
-    
-   
-    
-    if (currency && btc_alert_price){
+    var email = validated_contact_data[0]
+    var sms = validated_contact_data[1]
+
+    // Establish if the alert is for a higher price or a lower price
+    if (currency && btc_alert_price) {
         if (parseFloat(btc_value_now) > parseFloat(btc_alert_price)) {
             direction = " when its value drops below " + btc_alert_price + "BTC";
-        } else {
+        }
+        else {
             direction = " when its value has risen above " + btc_alert_price + "BTC";
         }
     };
-    
-    
-    console.log(direction);
-    
-    if (email_test && sms_test) {
+    // Update the alert message with the right contact data
+    if (email && sms) {
         $('#crypto-alert-create').text('Send me both SMS and EMAIL alerts for ' + currency + direction)
         return true;
     }
-    else if (email_test && !sms_test) {
+    else if (email && !sms) {
         $('#crypto-alert-create').text('Send me an EMAIL alert for ' + currency + direction)
         return true;
     }
-    else if (sms_test && !email_test) {
+    else if (sms && !email) {
         $('#crypto-alert-create').text('Send me an SMS alert for ' + currency + direction)
         return true;
     }
@@ -199,5 +225,12 @@ function email_and_sms_validator() {
         $('#crypto-alert-create').text('Please enter a delivery method to create an alert!')
         return false;
     };
+}
 
+
+function get_currency_rates(usd_value) {
+    $.getJSON('https://api.fixer.io/latest?base=USD', function(data) {
+        var cad_rate = data['rates']['CAD'] * usd_value;
+        $('.cad_value').text('$' + cad_rate + " CAD");
+    })
 }
